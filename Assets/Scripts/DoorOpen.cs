0c03db466a386e9a923d4928cbb5793486724ba2
unknown <tomiris.rad@gmail.com>
2020-03-10 08:42:55 -0600

    First draft of doors

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        anim.SetTrigger("OpenDoor");
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        anim.enabled = true;
+    }
+
+    void pauseAnimationEvent() 
+    {
+        anim.enabled = false;
+    }
+}
