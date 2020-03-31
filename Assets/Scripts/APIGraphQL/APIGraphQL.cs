e5d27031c650b26c9da221db772f75bd13a31665
Kyle Mollinet <kylemollinet@gmail.com>
2020-03-31 17:13:13 -0600

    Added working endpoint at beginning of PlayerMotor and a bunch of APIGraphQL files to the scripts folder

---
+++ Assets/Scripts/APIGraphQL/APIGraphQL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GraphQL {
+    public static class APIGraphQL {
+        private const string ApiURL = "https://vrbvlnshabb2nl55jwduzkb62e.appsync-api.us-west-2.amazonaws.com/graphql";
+        public static string Token = "da2-uw3aob4xl5c2lhz3zhbjtibmsi";
+
+        public static bool LoggedIn {
+            get { return !Token.Equals(""); } //todo: improve loggedin verification
+        } 
+
+        private static readonly GraphQLClient API = new GraphQLClient(ApiURL);
+
+        public static void Query (string query, object variables = null, Action<GraphQLResponse> callback = null) {
+            API.Query(query, variables, callback, Token);
+        }
+    }
+}
